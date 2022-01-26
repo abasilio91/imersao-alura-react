dab6d3337a81ca834c5c48205a18202ef2bb3b6f
Adam <you@example.comgit commit -m first>
2022-01-26 18:10:59 -0300

    final da aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+import React from 'react';
+import { useRouter } from 'next/router';
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -46,25 +19,13 @@ function Titulo(props) {
   );
 }
 
-// Componente React
-// function HomePage() {
-//     // JSX
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     )
-// }
-// export default HomePage
-
 export default function PaginaInicial() {
-  const username = 'abasilio91';
+  const [username, setUsername] = React.useState('');
+  const roteamento = useRouter();
+  // const [buttonDisabled, setButtonDisabled] = React.useState('True');
 
   return (
-    <>
-      <GlobalStyle />
+    <>  
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,17 +52,31 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (event) {
+              event.preventDefault();
+              roteamento.push('/chat');
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
+          <Titulo tag="h2">Boas vindas de volta!</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals['01'] }}>
               {appConfig.name}
             </Text>
 
             <TextField
+              value={username}
+              onChange={function(event) {
+                const valor = event.target.value;
+                setUsername(valor);
+                
+                // if (valor.length > 2) {
+                //   setButtonDisabled('False');
+                // };
+
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -116,6 +91,7 @@ export default function PaginaInicial() {
               type='submit'
               label='Entrar'
               fullWidth
+              disabled = {username.length < 3}
               buttonColors={{
                 contrastColor: appConfig.theme.colors.primary["05"],
                 mainColor: appConfig.theme.colors.primary["01"],
